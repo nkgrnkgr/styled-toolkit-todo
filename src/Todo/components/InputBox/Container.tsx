99fad0c449d04bfa2e910753e5d6c8f7051f25db
taiki-nishi <taiki-nishi@cybozu.co.jp>
2020-04-23 17:57:06 +0900

    ContainerにReduxとの繋ぎ込みを追加

--- src/Todo/components/InputBox/Container.tsx
+++ src/Todo/components/InputBox/Container.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import InputBox from './InputBox';
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../modules/types";
+import InputBox from "./InputBox";
+import { operations } from "../../modules/inputBox";
 
 const Container = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const props = {
-    onChange: () => {},
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+      dispatch(operations.changeText(e.target.value)),
     onClick: () => {},
   };
 
